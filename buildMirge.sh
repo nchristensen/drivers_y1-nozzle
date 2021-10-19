c737680f84916a358d04e550c95c396692a8170c
Mike Campbell <mtcampbe@illinois.edu>
2021-10-19 10:50:40 -0500

    Install y1-production, remove obviated merge.

--- buildMirge.sh
+++ buildMirge.sh
@@ -53,19 +53,10 @@ fi
 
 if [ -z ${CONDA_PATH+x} ]; then
   echo "CONDA_PATH unset, installing new conda with emirge"
-  echo "./install.sh --env-name=mirgeDriver.Y1nozzle $git_method --branch=parallel-lazy"
-  ./install.sh --env-name=mirgeDriver.Y1nozzle-lazy $git_method --branch=parallel-lazy
+  echo "./install.sh --env-name=mirgeDriver.Y1nozzle $git_method --branch=y1-production"
+  ./install.sh --env-name=mirgeDriver.Y1nozzle-lazy $git_method --branch=y1-production
 else
   echo "Using existing Conda installation, ${CONDA_PATH}"
-  echo "./install.sh --conda-prefix=$CONDA_PATH --env-name=mirgeDriver.Y1nozzle $git_method --branch=parallel-lazy"
-  ./install.sh --conda-prefix=$CONDA_PATH --env-name=mirgeDriver.Y1nozzle-lazy $git_method --branch=parallel-lazy
+  echo "./install.sh --conda-prefix=$CONDA_PATH --env-name=mirgeDriver.Y1nozzle $git_method --branch=y1-production"
+  ./install.sh --conda-prefix=$CONDA_PATH --env-name=mirgeDriver.Y1nozzle-lazy $git_method --branch=y1-production
 fi
-cd mirgecom
-git pull
-git checkout y1-production
-git checkout parallel-lazy
-git checkout -b mrgy1
-git config --global user.email "ci@github.com"
-git config --global user.name "CI Runner"
-git merge y1-production
-cd ../
